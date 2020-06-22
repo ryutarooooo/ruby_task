9821d9b826186c3a4d78d0e0e2ab6d86d00c203b
永田隆太朗 <nagatatakashitarou@mbp.local>
2020-06-22 22:00:04 +0900

    editing 01_3.rb 01_4.rb

--- 01_3.rb
+++ 01_3.rb
@@ -3,4 +3,4 @@ price = 10000
 # コードを追加
 print "人数を入力して下さい > "
 number = gets.to_i
-p "合計料金: ￥ #{number * price}"
+puts "合計料金: ￥ #{number * price}"
--- 01_4.rb
+++ 01_4.rb
@@ -4,9 +4,9 @@ price = 10000
 print "人数を入力して下さい > "
 number = gets.to_i
 if number >= 5
-  p "5人以上なので10%割引となります"
+  puts "5人以上なので10%割引となります"
   discount = number * price * 0.9
-  p "合計料金: ￥ #{discount.round}"
+  puts "合計料金: ￥ #{discount.round}"
 else
-  p "合計料金: ￥ #{number * price}"
+  puts "合計料金: ￥ #{number * price}"
 end
